100cdbcc9e25d50156b0895073d80b36af72e802
Danielton <danielton.ribeiro@fmd.ag>
2025-01-26 00:42:19 -0300

    feat(livros): cria rota de busca

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -5,10 +5,11 @@ const routes = express.Router();
 
 
 routes.get('/livros', LivroController.listarLivros);
+routes.get('/livros/busca', LivroController.listarLivrosPorEditora);
 routes.post('/livros', LivroController.cadastrarLivro);
 routes.get('/livros/:id', LivroController.listarLivroPorId);
 routes.put('/livros/:id', LivroController.atualizarLivro);
-routes.delete('/livros/:id', LivroController.atualizarLivro);
+routes.delete('/livros/:id', LivroController.apagarLivro);
 
 
 export default routes;
