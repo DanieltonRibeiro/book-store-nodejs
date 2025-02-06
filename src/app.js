c77d80c0c4d20c52652521bfc2b6fb1e8c56225f
Danielton <danielton.ribeiro@fmd.ag>
2025-02-05 22:40:53 -0300

    feat(app.js): importa o manipulador de erros e adiciona no express

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import conectaNaDatabase from './config/dbConnect.js';
 import routes from './routes/index.js';
+import { manipuladorDeErros } from './middlewares/manipuladorDeErros.js';
 
 
 const conexao = await conectaNaDatabase();
@@ -15,5 +16,8 @@ conexao.once("open", () => {
 
 const app = express();
 routes(app);
+
+app.use(manipuladorDeErros);
+
 
 export default app;
